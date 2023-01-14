054673aaccbd83c310dcdfeed6bcbc9ec499e5a7
aaronpeh022 <aaronpeh022@gmail.com>
2023-01-14 23:56:43 +0800

    added class 22

---
+++ classes.js
@@ -0,0 +1,40 @@
+class Boundary {
+    static width = 48
+    static height = 48
+    constructor({ position }) {
+        this.position = position
+        this.width = 48
+        this.height = 48
+    }
+
+    draw() {
+        c.fillStyle = 'rgba(255, 0, 0, 0.0)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+class Sprite {
+    constructor({ position, velocity, image, frame = { max: 1 } }) {
+        this.position = position
+        this.image = image
+        this.frame = frame
+        this.image.onload = () => {
+            this.width = this.image.width / this.frame.max
+            this.height = this.image.height
+        }
+    }
+
+    draw() {
+        c.drawImage(
+            this.image,
+            0,
+            0,
+            this.image.width / this.frame.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frame.max,
+            this.image.height
+        )
+    }
+}
