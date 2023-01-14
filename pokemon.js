11cf73d8012f6b5706964960ee6faa77ec89a1c7
aaronpeh022 <aaronpeh022@gmail.com>
2023-01-14 23:50:03 +0800

    added classes.js

--- pokemon.js
+++ pokemon.js
@@ -10,20 +10,7 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionMap.push(collisions.slice(i, i + 70))
 }
 
-class Boundary {
-    static width = 48
-    static height = 48
-    constructor({ position }) {
-        this.position = position
-        this.width = 48
-        this.height = 48
-    }
 
-    draw() {
-        c.fillStyle = 'rgba(255, 0, 0, 0.0)'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
 
 const boundaries = []
 const offset = {
@@ -54,31 +41,7 @@ image.src = "./img/Pellet Town.png"
 const playerImage = new Image()
 playerImage.src = "./img/playerDown.png"
 
-class Sprite {
-    constructor({ position, velocity, image, frame = { max: 1 } }) {
-        this.position = position
-        this.image = image
-        this.frame = frame
-        this.image.onload = () => {
-            this.width = this.image.width / this.frame.max
-            this.height = this.image.height
-        }
-    }
 
-    draw() {
-        c.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frame.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frame.max,
-            this.image.height
-        )
-    }
-}
 
 const player = new Sprite({
     position: {
